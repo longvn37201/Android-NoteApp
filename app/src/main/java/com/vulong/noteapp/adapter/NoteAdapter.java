c9881a585fe14f2366af3df097b3c7b1462d439e
VU LONG <vungoclong2504@gmail.com>
2021-05-16 15:38:25 +0700

    remove RoundedImageView to fix lose image in recyclerview, fix NestedSrollView not scrolling with keyboard on

--- app/src/main/java/com/vulong/noteapp/adapter/NoteAdapter.java
+++ app/src/main/java/com/vulong/noteapp/adapter/NoteAdapter.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,7 +17,6 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.makeramen.roundedimageview.RoundedImageView;
 import com.vulong.noteapp.R;
 import com.vulong.noteapp.entities.Note;
 import com.vulong.noteapp.listeners.NoteAdapterListener;
@@ -69,7 +70,7 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> im
         Note note = noteListFiltered.get(position);
         holder.setNote(note);
 
-        holder.constraintLayout.setOnClickListener(v -> {
+        holder.linearLayout.setOnClickListener(v -> {
             noteAdapterListener.onNoteSelected(position, noteListFiltered.get(position));
         });
 
@@ -125,28 +126,38 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> im
         private TextView tvTitle;
         private TextView tvContent;
         private TextView tvDateTime;
-        private ConstraintLayout constraintLayout;
-        private RoundedImageView imgImage;
+        private LinearLayout linearLayout;
+//        private RoundedImageView imgImage;
+        private ImageView imgImage;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tv_title);
             tvContent = itemView.findViewById(R.id.tv_content);
             tvDateTime = itemView.findViewById(R.id.tv_date_time_in_item);
-            constraintLayout = itemView.findViewById(R.id.layout_item);
+            linearLayout = itemView.findViewById(R.id.layout_item);
             imgImage = itemView.findViewById(R.id.img_rounded);
         }
 
         private void setNote(Note note) {
             tvTitle.setText(note.getTitle());
-            tvContent.setText(note.getContent());
+            if(note.getContent().equals("")||note.getContent()==null){
+                tvContent.setVisibility(View.GONE);
+            }else{
+                tvContent.setVisibility(View.VISIBLE);
+                tvContent.setText(note.getContent());
+            }
             tvDateTime.setText(note.getDateTime());
-            constraintLayout.getBackground().setColorFilter(note.getColor(), PorterDuff.Mode.SRC_ATOP);
+            linearLayout.getBackground().setColorFilter(note.getColor(), PorterDuff.Mode.SRC_ATOP);
+
+            //todo
             if(note.getImagePath()!=null){
                 try {
                     File file = new File(note.getImagePath());
                     if (file.exists()) {
-                        Glide.with(imgImage.getContext()).load(note.getImagePath()).into(imgImage);
+                        Glide.with(imgImage.getContext()).load(file).into(imgImage);
+//                        Picasso.get().load(file).resize(300,).centerCrop().into(imgImage);
+                        imgImage.setVisibility(View.VISIBLE);
                     }else{
                         imgImage.setVisibility(View.GONE);
                     }
@@ -155,6 +166,9 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> im
                     imgImage.setVisibility(View.GONE);
 
                 }
+            }else{
+                imgImage.setVisibility(View.GONE);
+
             }
 
 
